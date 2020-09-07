f4741d499685d79daebba6469a6264f14eea491d
전표수 <snstkfka02@hanmail.net>
2020-09-08 02:33:49 +0900

    favicon title && mobile update

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MailOutlined, PhoneOutlined, GithubOutlined, InstagramOutlined} from '@ant-design/icons';
+import { MailOutlined, PhoneOutlined, GithubOutlined} from '@ant-design/icons';
 import '../CSSs/Photo.css';
 function Photo() {
     return (
@@ -12,7 +12,6 @@ function Photo() {
                         <p className="text_line2"><MailOutlined className="IconImage"/>Snstkfka02@Hanmail.net</p>
                         <p className="text_line2"><PhoneOutlined className="IconImage"/>010 - 2999 - 3667</p>
                         <p className="text_line2"><GithubOutlined className="IconImage"/><a href="https://github.com/Pyosoo" target="_blank">Github.com/Pyosoo</a></p><br/>
-                        <p className="text_line2"><InstagramOutlined className="IconImage"/><a href="https://instagram.com/Pyojeon" target="_blank">PyoJeon</a></p>
                     </div>
                 </div>
             </div>
