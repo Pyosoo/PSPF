4d845495bbb4ecac71222804d7a586c0286935ce
전표수 <snstkfka02@hanmail.net>
2020-11-28 17:52:47 +0900

    blog added

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MailOutlined, PhoneOutlined, GithubOutlined} from '@ant-design/icons';
+import { MailOutlined, PhoneOutlined, GithubOutlined, ReadOutlined} from '@ant-design/icons';
 import '../CSSs/Photo.css';
 function Photo() {
     return (
@@ -12,6 +12,7 @@ function Photo() {
                         <p className="text_line2"><MailOutlined className="IconImage"/>Snstkfka02@Hanmail.net</p>
                         <p className="text_line2"><PhoneOutlined className="IconImage"/>010 - 2999 - 3667</p>
                         <p className="text_line2"><GithubOutlined className="IconImage"/><a href="https://github.com/Pyosoo" target="_blank">Github.com/Pyosoo</a></p><br/>
+                        <p className="text_line2"><ReadOutlined className="IconImage"/><a href="https://pyolog.tistory.com/" target="_blank">Blog</a></p>
                     </div>
                 </div>
             </div>
